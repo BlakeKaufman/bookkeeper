3f6654a6aacadb845af08379533c4228aba82107
Blake Kaufman <68204898+BlakeKaufman@users.noreply.github.com>
2023-07-28 17:46:41 -0400

    Add files via upload

--- src/components/loginPathBack/index.jsx
+++ src/components/loginPathBack/index.jsx
@@ -3,9 +3,11 @@ import "./index.css";
 import backIcon from "../../assets/images/icons/angle-small-left.svg";
 
 export default function LoadBackButton(props) {
-  console.log(props);
   return (
-    <div id="back_Container">
+    <div
+      onClick={!!props.toggleFunction ? props.toggleFunction : () => null}
+      id="back_Container"
+    >
       <div className="icon">
         <img src={backIcon} alt="backarrow for back button" />
       </div>
