c7d73e7b5da6978e238948d5a145f03863d41223
Blake Kaufman <68204898+BlakeKaufman@users.noreply.github.com>
2023-08-01 18:14:36 -0400

    Add files via upload

--- client/components/loadingAnimation/index.jsx
+++ client/components/loadingAnimation/index.jsx
@@ -2,10 +2,9 @@ import "./index.css";
 
 export default function LoadingAnimation(props) {
   const style = {
-    display: props.isDisplayed.length === 0 ? "inline-block" : "none",
+    display: props.isDisplayed ? "inline-block" : "none",
   };
 
-  console.log(props.isDisplayed.length);
   return (
     <div style={style} className="lds-ring">
       <div></div>
