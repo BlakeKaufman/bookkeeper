c5c97e553fdcd2fb6ceb59a7941238090cc8b116
Blake Kaufman <68204898+BlakeKaufman@users.noreply.github.com>
2023-08-01 18:16:17 -0400

    Add files via upload

--- netlify/database_injection.js
+++ netlify/database_injection.js
@@ -1,7 +1,41 @@
+const express = require("express");
+const mysql = require("mysql2");
+const DBinformation = require("../databaseInformation.json");
+const app = express();
+
+// Create a database connection pool
+const connection = mysql.createConnection({
+  host: DBinformation.host,
+  user: DBinformation.user,
+  password: DBinformation.pass,
+  database: DBinformation.database,
+});
+// app.use(express.json());
+
 export async function handler(event, context) {
-  console.log(event.body);
+  const { bookISBN } = JSON.parse(event.body);
+
+  // Insert the data into the database
+  connection.query(
+    "INSERT INTO books (isbn) VALUES (?)",
+    bookISBN,
+    (err, result) => {
+      console.log(err, result);
+      // if (err) {
+      //   console.error("Error inserting data:", err);
+      //   // res.status(500).send("Error inserting data");
+      // } else {
+      //   console.log("Data inserted successfully");
+      //   // res.status(201).json({
+      //   //   message: "Data inserted successfully",
+      //   //   postId: result.insertId,
+      //   // });
+      // }
+    }
+  );
+
   return {
     statusCode: 200,
-    body: JSON.stringify(event.body),
+    body: JSON.stringify("testing"),
   };
 }
