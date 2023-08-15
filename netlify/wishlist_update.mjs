922a029cc598f921b611b02d6f222c62948a8c81
Blake Kaufman <68204898+BlakeKaufman@users.noreply.github.com>
2023-08-14 21:28:39 -0400

    Add files via upload

---
+++ netlify/wishlist_update.mjs
@@ -0,0 +1,70 @@
+// const express = require("express");
+// const mysql = require("mysql2");
+import DBinformation from "../databaseInformation.json";
+import { MongoClient, ServerApiVersion } from "mongodb";
+// const app = express();
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${DBinformation.user}:${DBinformation.pass}@${DBinformation.user}.zgq19jn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+export async function handler(event, context) {
+  const newData = event.body ? JSON.parse(event.body) : null; //sanitation
+
+  // Sample data to be inserted
+
+  if (!newData) {
+    // Handle empty request body or invalid JSON format
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Invalid request body or empty payload" }),
+    };
+  }
+  try {
+    // Connect to the MongoDB server
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    // Access the database and collection
+    const database = client.db("Users"); // Replace 'mydatabase' with your database name
+    const collection = database.collection("wishlist"); // Replace 'mycollection' with your collection name
+
+    const wishlist = await collection.findOne({ user: newData.user });
+
+    if (wishlist) {
+      const result = await collection.updateOne(
+        {
+          user: newData.user,
+        },
+        {
+          $set: { wishlist: newData.wishlist },
+        },
+        { upsert: true }
+      );
+
+      // Sending back
+      return {
+        statusCode: 200,
+        body: JSON.stringify("Document updated successfully"),
+      };
+    } else {
+      // Insert the data
+      const result = await collection.insertOne(newData);
+      console.log("Data inserted successfully:", result.insertedId);
+      return {
+        statusCode: 200,
+        body: JSON.stringify(result.insertedId),
+      };
+    }
+  } catch (error) {
+    console.error("Error inserting data:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  } finally {
+    // Close the connection when done
+    client.close();
+  }
+}
